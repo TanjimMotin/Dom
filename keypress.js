1853085865bb740a78248c6a710318aa9b3c56f7
TanjimMotin <tanjimotin@gmail.com>
2018-09-23 22:51:40 -0400

    finish

--- keypress.js
+++ keypress.js
@@ -1,5 +1,5 @@
-var keyPressH1 = document.getElementById("keyPressShown");
 
+var keyPressH1 = document.getElementById("keyPressLetter");
 
 document.body.onkeypress = function(){
 	keyPressH1.innerHTML = event.key;
