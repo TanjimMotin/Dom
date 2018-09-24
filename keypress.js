533a5856e110df77b2f9cd721b6b5f6655d38c33
TanjimMotin <tanjimotin@gmail.com>
2018-09-24 11:26:38 -0400

    mor changes

--- keypress.js
+++ keypress.js
@@ -1,9 +1,7 @@
 
 var keyPressH1 = document.getElementById("keyPressLetter");
 
-/*document.body.onkeypress   = function(){
-	keyPressH1.innerHTML  += event.key;
-}*/
+
 document.addEventListener('keypress', function(e){
 	keyPressH1.innerHTML  = e.key;
 })
